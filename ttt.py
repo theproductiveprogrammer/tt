5bbb2b058ebd79d43b291bb1736fbe039971993b
Charles L <charles.lobo@gmail.com>
2022-09-17 21:05:33 +1000

    feat(ttt): start tracking

--- ttt.py
+++ ttt.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+from datetime import datetime, timezone
 
 TRACKING_FILE = os.path.expanduser("~/.tracking")
 
@@ -20,6 +21,26 @@ def main():
     else:
         show_help()
 
+
+def start_tracking(recs, what):
+    rec = recs.get(what)
+
+    if rec and rec[-1]:
+        (t,dt) = rec[-1]
+        if t == "+":
+            tm = datetime.now(timezone.utc) - dt
+            tm = str(tm)
+            ndx = tm.rfind(".")
+            if ndx != -1:
+                tm = tm[:ndx]
+            print(what + " already being tracked - " + tm)
+            return
+
+    now = datetime.isoformat(datetime.now(timezone.utc))
+    with open(TRACKING_FILE, 'a') as f:
+        rec = "+" + now + "\t" + what
+        f.write(rec + "\n")
+        print("started: " + what)
 
 def show_help():
     print("ttt.py - Track time")
